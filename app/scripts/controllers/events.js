2d7508a9d7f6069aa960d6681390e0aa51fa1bc7
zhao <zhaoyudong_0129@126.com>
2015-12-03 15:57:28 +0800

    event

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -8,7 +8,18 @@
  * Controller of the yeodjangoApp
  */
 angular.module('yeodjangoApp')
-  .controller('EventsCtrl',['$scope','EventNoDetail','eventPaginator', function ($scope,EventNoDetail,eventPaginator) {
+  .controller('EventsCtrl', ['$scope', 'EventNoDetail', 'eventPaginator', function ($scope, EventNoDetail, eventPaginator) {
     $scope.eventPaginator = eventPaginator;
-    $scope.eventPaginator.watch($scope,'eventPaginator.currentPage');
+    $scope.eventPaginator.watch($scope, 'eventPaginator.currentPage');
+
+    $scope.slides = [
+      {
+        image: '/images/hero-domain-story.jpg',
+        text: 'slide 1'
+      },
+      {
+        image: '/images/hero-rogervoice-story.jpg',
+        text: 'slide 2'
+      }
+    ];
   }]);
