97dd0b859eef0846cbc081163102d609e4ef762b
Zhou Li <zhoulijoe@gmail.com>
2015-12-07 14:45:30 +0800

    Login page add invalidation and spinner when submitting

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,6 +4,11 @@ angular.module('xbertsApp')
   .controller('LoginCtrl', ['$scope', '$location', 'AuthService', 'urlUtil',
     function($scope, $location, AuthService, urlUtil) {
     $scope.login = function() {
+      if (!$scope.loginForm.$valid) {
+        return;
+      }
+      $scope.$emit('backdropOn', 'post');
+
       // TODO: To make it work with django login redirect, need to extract potential next query param and pass it on
       // to auth request. Remove after implementing post-auth redirect in frontend.
       // ex. http://localhost:9000/?next=/projects/launch/#/login
@@ -20,9 +25,13 @@ angular.module('xbertsApp')
         function(value, responseHeaders) {
           AuthService.setUser(value);
 
+          $scope.$emit('backdropOff', 'success');
+
           window.location.href = '/auth/home/';
         },
         function(httpResponse) {
+          $scope.$emit('backdropOff', 'error');
+
           if (httpResponse.status === 401 || httpResponse.status === 403) {
             $scope.loginError = {invalidCredentials: true};
           } else {
