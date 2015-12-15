a762341501945cb64f4f10568d2b262b732973f1
zhao <zhaoyudong_0129@126.com>
2015-12-15 14:13:58 +0800

    landing carosaul

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,19 +15,32 @@ angular.module('xbertsApp')
       $scope.expertPaginator = expertPaginator;
       $scope.slides = [
         {
+          image: '/images/landing_3.jpg',
+          text: '&nbsp;<br/>&nbsp;',
+          buttonText:'Apply Now',
+          buttonColor:'btn-primary',
+          url:'/review/1/applicant/'
+        },
+        {
+          image: '/images/landing_4.jpg',
+          text: '&nbsp;<br/>&nbsp;',
+          buttonText:'Apply Now',
+          buttonColor:'btn-primary',
+          url:'/review/2/applicant/'
+        },
+        {
+          image: '/images/landing_2_2.jpg',
+          text: '&nbsp;<br/>&nbsp;',
+          buttonText:'Meet Us',
+          buttonColor:'btn-primary',
+          url:'/#/events/1'
+        },
+        {
           image: '/images/landing_1_1.jpg',
           text: 'Connect smart hardware innovators with distributors,<br/>retailers and sales agent partners around the world',
           buttonText:'Get Started',
           buttonColor:'btn-primary',
           url:'/#/signup'
-        },
-
-        {
-          image: '/images/loading_2.jpg',
-          text: '&nbsp;<br/>&nbsp;',
-          buttonText:'Meet Us',
-          buttonColor:'btn-primary',
-          url:'/#/events/1'
         }
       ];
     }]);
