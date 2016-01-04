c90fbdd738272a16621693d5de6d75d846228a99
zhao <zhaoyudong_0129@126.com>
2016-01-04 16:01:57 +0800

    project search sessionstorage

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -8,11 +8,14 @@
  * Controller of the xbertsApp
  */
 angular.module('xbertsApp')
-  .controller('ProjectsCtrl', ['$scope', 'projectPaginator', 'SystemData', function ($scope, projectPaginator, SystemData) {
+  .controller('ProjectsCtrl', ['$scope', 'projectPaginator', 'SystemData','localStorageService', function ($scope, projectPaginator, SystemData,localStorageService) {
     $scope.projectTypes = SystemData.getProjectTypes();
     $scope.projectPaginator = projectPaginator;
     $scope.projectPaginator.watch($scope, 'projectPaginator.currentPage');
-    $scope.projects = {ordering: '-date_published'};
+    $scope.projects = {
+      project_category_id: localStorageService.get('project_search_type') || '',
+      ordering: localStorageService.get('project_search_order') || '-date_published'
+    };
     $scope.onSearch = function () {
       $scope.projectPaginator.params = $scope.projects;
       $scope.projectPaginator.items = [];
@@ -20,4 +23,8 @@ angular.module('xbertsApp')
       $scope.projectPaginator.next = 'true';
       $scope.projectPaginator.loadNext();
     };
+    $scope.$watch('projects.project_category_id + projects.ordering', function () {
+      localStorageService.set('project_search_type', $scope.projects.project_category_id);
+      localStorageService.set('project_search_order', $scope.projects.ordering);
+    });
   }]);
