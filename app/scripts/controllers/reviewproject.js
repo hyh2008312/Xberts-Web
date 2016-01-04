7bce6371e67570dc1f5028128c612537d27fca04
zhao <zhaoyudong_0129@126.com>
2016-01-04 14:32:08 +0800

    reviews

--- app/scripts/controllers/reviewproject.js
+++ app/scripts/controllers/reviewproject.js
@@ -14,14 +14,26 @@ angular.module('xbertsApp')
     $scope.sub = {
       subNavShow: false
     };
+    $scope.isOutDated = function (time) {
+      return new Date(time) - Date.now() > 0;
+    };
+    var section2 = angular.element(document.getElementById('section-2'));
     var section3 = angular.element(document.getElementById('section-3'));
+    var section4 = angular.element(document.getElementById('section-4'));
+    var section5 = angular.element(document.getElementById('section-5'));
+    $scope.toSection4 = function () {
+      $document.scrollToElementAnimated(section4);
+    };
+    $scope.toSection5 = function () {
+      $document.scrollToElementAnimated(section5);
+    };
     $scope.toSection3 = function () {
       $document.scrollToElementAnimated(section3);
     };
-    var section2 = angular.element(document.getElementById('section-3'));
     $scope.toSection2 = function () {
       $document.scrollToElementAnimated(section2);
     };
+
     $rootScope.$on('duScrollspy:becameActive', function ($event, $element, $target) {
       if ($target.context.id === 'section-1') {
         $scope.$apply(function () {
