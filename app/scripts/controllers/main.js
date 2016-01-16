058282a5daeb71b31464c5b1b69f18bab07211d9
zhao <zhaoyudong_0129@126.com>
2016-01-16 11:38:30 +0800

    revoke ces

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,6 @@ angular.module('xbertsApp')
       $scope.expertPaginator = expertPaginator;
       $scope.slides = [
         {
-          image: '/images/landing_01.jpg',
-          text: '&nbsp;<br/>&nbsp;',
-          buttonText:'Meet Us',
-          buttonColor:'btn-primary',
-          url: 'application.event({eventId: 1})'
-        },
-        {
-          image: '/images/landing_02.jpg',
-          text: '&nbsp;<br/>&nbsp;',
-          buttonText:'Vote Now',
-          buttonColor:'btn-primary',
-          url: 'application.vote({eventId: 1})'
-        },
-        {
           image: '/images/landing_1_1.jpg',
           text: 'Connect smart hardware innovators with distributors,<br/>retailers and sales agent partners around the world',
           buttonText:'Get Started',
