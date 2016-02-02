50d2b90e96bd5c66eec3e01e0be92218260b07d1
Zhou Li <zhoulijoe@gmail.com>
2016-02-02 16:04:03 +0800

    Improve linkedin login user experience

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,21 +1,24 @@
 'use strict';
 
 angular.module('xbertsApp')
-  .controller('ApplicationCtrl', ['$scope', '$rootScope', '$state', '$auth', 'AuthService',
-    function($scope, $rootScope, $state, $auth, AuthService) {
+  .controller('ApplicationCtrl', ['$scope', '$rootScope', '$state', '$auth', '$interval', 'AuthService',
+    'SatellizerPopup',
+    function($scope, $rootScope, $state, $auth, $interval, AuthService,
+             SatellizerPopup) {
       $scope.linkedinError = {};
 
       $scope.logout = function() {
         $rootScope.$emit('logout', true);
       };
 
-      $scope.linkedinLogin = function() {
+      $scope.linkedinLogin = function(form) {
+        $scope.$emit('backdropOn', 'post');
+
         $scope.linkedinError = {};
 
-        $auth.authenticate('linkedin')
-          .then(function(response) {
-            $scope.$emit('backdropOn', 'post');
+        var authPromise = $auth.authenticate('linkedin');
 
+        authPromise.then(function(response) {
             return AuthService.exchangeLinkedinToken(response.data.access_token);
           })
           .then(function(value) {
@@ -24,8 +27,21 @@ angular.module('xbertsApp')
           .catch(function(response) {
             $scope.$emit('backdropOff', 'error');
 
-            $scope.linkedinError.generic = true;
+            form.serverError = {linkedinError: true};
           });
+
+        // Need to hide spinner when linkedin popup window is manually closed. In this case social auth promise
+        // has not been resolved.
+        var polling = $interval(function() {
+          if (!SatellizerPopup.popupWindow || SatellizerPopup.popupWindow.closed ||
+               SatellizerPopup.popupWindow.closed === undefined) {
+            $interval.cancel(polling);
+
+            if (authPromise.$$state.status === 0) {
+              $scope.$emit('backdropOff', 'close');
+            }
+          }
+        }, 1000);
       };
     }]);
 
