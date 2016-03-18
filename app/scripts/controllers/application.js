98678c7d81678dc86c2800f0b4ab0833518a6051
Zhou Li <zhoulijoe@gmail.com>
2016-03-18 18:48:31 +0800

    Track linkedin login and logout as virtual page

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -1,16 +1,20 @@
 'use strict';
 
 angular.module('xbertsApp')
-  .controller('ApplicationCtrl', ['$scope', '$rootScope', '$state', '$interval', 'AuthService',
-    function($scope, $rootScope, $state, $interval, AuthService) {
+  .controller('ApplicationCtrl', ['$scope', '$rootScope', '$state', '$interval', 'AuthService', 'AnalyticsService',
+    function($scope, $rootScope, $state, $interval, AuthService, AnalyticsService) {
       $scope.linkedinError = {};
 
       $scope.logout = function() {
+        AnalyticsService.sendPageView('/logout');
+
         $rootScope.$emit('logout', true);
       };
 
       $scope.linkedinLogin = function() {
         $scope.$emit('backdropOn', 'post');
+
+        AnalyticsService.sendPageView('/linkedinlogin');
 
         AuthService.linkedinLogin();
       };
