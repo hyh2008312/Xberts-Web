15bc1c39a64180e5bf53a20b31573f3bf03140af
Zhou Li <zhoulijoe@gmail.com>
2016-07-01 00:58:37 -0700

    Stop tracking build dist dir

--- dist/scripts/override-configuration.28c7e746.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('configuration.properties')
-  .constant('overrideConfiguration', {
-    apiBaseUrl: 'http://localhost:8000/api',
-
-    oauthClientId: 'CwWuk2YE1v2LZIyKy2mvwyuhliYKHkKSxfhb0835'
-  });
-
