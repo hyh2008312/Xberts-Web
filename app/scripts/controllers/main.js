95b546baaeb468dbf4d5148d9a2c1c6fe157332d
zhao <zhaoyudong_0129@126.com>
2016-07-19 13:52:31 +0800

    change carousel padding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,19 +23,19 @@ angular.module('xbertsApp')
           subtitle: "Xberts is a global community for early adopters to explore new creative products and share their experience with others",
           buttonText: 'Get Started',
           buttonColor: 'button-primary',
-          captionLocation: '',
           url: 'application.crowdtestings',
           params: {}
         }
-        //, {
-        //  id:1,
-        //  image: '/images/jisiwei.jpg',
-        //  buttonText: 'Apply Now',
-        //  buttonColor: 'button-primary',
-        //  captionLocation: '',
-        //  url: 'application.crowdtesting',
-        //  params: {reviewId: 29}
-        //}
+        , {
+          id:1,
+          image: '/images/sensative.jpg',
+          title: 'Sensative is calling 20 pioneers for crowdtesting',
+          subtitle: "Application Deadline<br/>12am PST - Aug 05, 2016 ",
+          buttonText: 'Apply Now',
+          buttonColor: 'button-primary',
+          url: 'application.crowdtesting',
+          params: {reviewId: 29}
+        }
       ];
 
       $scope.slideButtonClick = function (slide) {
