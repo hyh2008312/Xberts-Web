6acec43bafae8a59951d1ed75c7e1865953e6768
zhao <zhaoyudong_0129@126.com>
2016-08-02 17:15:42 +0800

    add banner to campaing list

--- app/scripts/controllers/review/review-list.js
+++ app/scripts/controllers/review/review-list.js
@@ -15,9 +15,43 @@ angular.module('xbertsApp')
 
       $scope.buyNow = function (id, $event) {
         $state.go('application.campaign', {reviewId: id, action: 'buy'});
-      }
+      };
 
       $scope.applyNow = function (id, $event) {
         $state.go('application.protected.apply', {reviewId: id});
-      }
+      };
+
+      $scope.active = 0;
+      $scope.slides = [
+        {
+          id: 0,
+          image: '/images/Emie_lamp.jpg',
+          title: 'This adorable connected lamp brings brightness, warmth and companion to you',
+          subtitle: "",
+          buttonText: 'Try Me Now',
+          buttonColor: 'button-primary',
+          url: 'application.campaign',
+          params: {reviewId: 31}
+        }
+        , {
+          id: 1,
+          image: '/images/Emie_Watch.jpeg',
+          title: 'Stay on top of your health and fitness goals, without compromising on your sense of style',
+          subtitle: "",
+          buttonText: 'Try Me Now',
+          buttonColor: 'button-primary',
+          url: 'application.campaign',
+          params: {reviewId: 31}
+        }
+        , {
+          id: 2,
+          image: '/images/Jorno_keyboard.jpg',
+          title: 'Just your type. Check out this ultra-slim compact folding keyboard',
+          subtitle: "",
+          buttonText: 'Try Me Now',
+          buttonColor: 'button-primary',
+          url: 'application.campaign',
+          params: {reviewId: 32}
+        }
+      ];
     }]);
