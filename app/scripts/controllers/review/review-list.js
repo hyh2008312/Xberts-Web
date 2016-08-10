c3ffc2bb48eeefacc0ef69eb1317c2ed9c2c117e
Zhou Li <zhoulijoe@gmail.com>
2016-08-09 17:19:33 -0700

    Add main banner to home page
    - Update text on review list and details page

--- app/scripts/controllers/review/review-list.js
+++ app/scripts/controllers/review/review-list.js
@@ -26,30 +26,41 @@ angular.module('xbertsApp')
       $scope.slides = [
         {
           id: 0,
-          image: '/images/Emie_lamp.jpg',
+          image: 'https://xberts.imgix.net/static/banner/engineer_adjusting_3d_printer.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=c5544734db8e38f9c68e9be878867179',
+          title: 'Early Access to Creative Products',
+          subtitle: 'Get early bird offers on latest gadgets and try pre-launched innovations for free.',
+          buttonShow: !$rootScope.user.isAuth(),
+          buttonText: 'Join Now',
+          buttonColor: 'btn-primary',
+          url: 'application.signup',
+          params: {}
+        }, {
+          id: 1,
+          image: 'https://xberts.imgix.net/static/banner/emie_lamp.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=0f434daa97bbf3c5f68e3c57c7d7ac02',
           title: 'Elfy Connected Lamp',
           subtitle: 'This adorable connected lamp brings brightness, warmth and companion to you.',
-          buttonText: 'Shop Now',
+          buttonShow: true,
+          buttonText: 'Get It Now',
           buttonColor: 'btn-primary',
           url: 'application.campaign',
           params: {reviewId: 47}
-        }
-        , {
-          id: 1,
-          image: '/images/Emie_Watch.jpg',
+        }, {
+          id: 2,
+          image: 'https://xberts.imgix.net/static/banner/emie_watch.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=3019acdc204f7120f9c5a767e0b98b5c',
           title: 'Nevo Balade Parisienne Smart Watch',
           subtitle: 'Stay on top of your health and fitness goals, without compromising on your sense of style.',
-          buttonText: 'Shop Now',
+          buttonShow: true,
+          buttonText: 'Get It Now',
           buttonColor: 'btn-primary',
           url: 'application.campaign',
           params: {reviewId: 48}
-        }
-        , {
-          id: 2,
-          image: '/images/Jorno_keyboard.jpg',
+        }, {
+          id: 3,
+          image: 'https://xberts.imgix.net/static/banner/jorno_keyboard.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=37e53a8accf2a74e6ed34579a239b1fc',
           title: 'Jorno Folding Bluetooth Keyboard',
           subtitle: 'Just your type. Check out this ultra-slim compact folding keyboard.',
-          buttonText: 'Shop Now',
+          buttonShow: true,
+          buttonText: 'Get It Now',
           buttonColor: 'btn-primary',
           url: 'application.campaign',
           params: {reviewId: 49}
