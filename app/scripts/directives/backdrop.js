132718ac869aeb4a28f166a6abbbd7028028bbfb
zhao <zhaoyudong_0129@126.com>
2016-08-12 18:52:32 +0800

    alter backdrop plugin

--- app/scripts/directives/backdrop.js
+++ app/scripts/directives/backdrop.js
@@ -57,7 +57,7 @@ angular.module('xbertsApp')
       }
     };
   }])
-  .directive('firstPop', ['$rootScope','$cookies','$uibModal', function ($rootScope,$cookies,$uibModal) {
+  .directive('firstPop', ['$rootScope', '$cookies', '$uibModal', function ($rootScope, $cookies, $uibModal) {
     return {
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
@@ -70,7 +70,7 @@ angular.module('xbertsApp')
 
 
         var pop = function () {
-          var isFirst=$cookies.get('isFirst');
+          var isFirst = $cookies.get('isFirst');
           if (isFirst) {
             return;
           }
@@ -87,26 +87,81 @@ angular.module('xbertsApp')
       }
     };
   }])
-  .directive('backToTop', ['$rootScope', function ($rootScope,$cookies,$uibModal) {
+  .directive('backToTop', [function () {
     return {
-      template: '<div class="xb-back-to-top">' +
-      '<button class="btn btn-primary btn-sm" ng-click="goToTop()"><i class="fa fa-arrow-up"></i></button> ' +
-      '</div>',
       restrict: 'E',
+      transclude: true,
       replace: true,
-      link: function postLink(scope, element, attrs) {
-        //element.addClass('backdrop');
-        //element.height(window.screen.availHeight);
-        //element.width(window.screen.availWidth);
+      template: '<div id="backtop" class="{{theme}}">' +
+      '<button class="btn btn-primary btn-sm"><i class="fa fa-arrow-up"></i> {{ text }}</button> ' +
+      '</div>',
+      scope: {
+        text: "@buttonText",
+        speed: "@scrollSpeed",
+        theme: "@buttonTheme"
+      },
+      link: function (scope, element) {
 
-        //element.addClass('hide');
-        //$rootScope.backdropCount = 0;
+        scope.text = scope.text || 'Scroll top';
+        scope.speed = parseInt(scope.speed, 10) || 300;
+        scope.theme = scope.theme || 'light';
 
+        var self = this;
 
-        scope.goToTop=function(){
-          document.body.scrollTop = document.documentElement.scrollTop = 0;
+        scope.currentYPosition = function () {
+          if (self.pageYOffset)
+            return self.pageYOffset;
+          if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+          if (document.body.scrollTop)
+            return document.body.scrollTop;
+          return 0;
         };
 
+        scope.smoothScroll = function () {
+          var startY = scope.currentYPosition();
+          var stopY = 0;
+          var distance = stopY > startY ? stopY - startY : startY - stopY;
+          if (distance < 100) {
+            scrollTo(0, stopY);
+            return;
+          }
+          var speed = Math.round(scope.speed / 100);
+          var step = Math.round(distance / 25);
+          var leapY = stopY > startY ? startY + step : startY - step;
+          var timer = 0;
+          if (stopY > startY) {
+            for (var i = startY; i < stopY; i += step) {
+              setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+              leapY += step;
+              if (leapY > stopY) leapY = stopY;
+              timer++;
+            }
+            return;
+          }
+          for (var j = startY; j > stopY; j -= step) {
+            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+            leapY -= step;
+            if (leapY < stopY) leapY = stopY;
+            timer++;
+          }
+        };
+
+        scope.button = element.find('button');
+
+        scope.button.on('click', function () {
+          scope.smoothScroll();
+          element.removeClass('show');
+        });
+
+        window.addEventListener('scroll', function () {
+          if (window.pageYOffset > 0) {
+            element.addClass('show');
+          } else {
+            element.removeClass('show');
+          }
+        });
       }
     };
+
   }]);
