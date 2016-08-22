cc4a762d3d9147f72325b5cc37a4932729ce55d1
zhao <zhaoyudong_0129@126.com>
2016-08-22 16:13:18 +0800

    change about page style

--- app/scripts/controllers/leave-prompt.js
+++ app/scripts/controllers/leave-prompt.js
@@ -15,4 +15,5 @@ angular.module('xbertsApp')
   .controller('AboutCtrl', ['$scope', '$rootScope',
     function ($scope, $rootScope) {
       $rootScope.pageSettings.setTitle('About Xberts');
+      $rootScope.pageSettings.setBackgroundColor('background-bg-gray');
     }]);
