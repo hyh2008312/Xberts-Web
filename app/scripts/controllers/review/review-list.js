d8d82e3abd31edfc80090f10846878df4401e5f7
zhao <zhaoyudong_0129@126.com>
2016-09-06 17:59:56 +0800

    add report amount to review detail page

--- app/scripts/controllers/review/review-list.js
+++ app/scripts/controllers/review/review-list.js
@@ -33,37 +33,38 @@ angular.module('xbertsApp')
           buttonColor: 'btn-primary',
           url: 'application.signup',
           params: {}
-        }, {
-          id: 1,
-          image: 'https://xberts.imgix.net/static/banner/emie_lamp.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=0f434daa97bbf3c5f68e3c57c7d7ac02',
-          title: 'Elfy Connected Lamp',
-          subtitle: 'This adorable connected lamp brings brightness, warmth and companion to you.',
-          buttonShow: true,
-          buttonText: 'Get It Now',
-          buttonColor: 'btn-primary',
-          url: 'application.campaign',
-          params: {reviewId: 47}
-        }, {
-          id: 2,
-          image: 'https://xberts.imgix.net/static/banner/emie_watch.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=3019acdc204f7120f9c5a767e0b98b5c',
-          title: 'Nevo Balade Parisienne Smart Watch',
-          subtitle: 'Stay on top of your health and fitness goals, without compromising on your sense of style.',
-          buttonShow: true,
-          buttonText: 'Get It Now',
-          buttonColor: 'btn-primary',
-          url: 'application.campaign',
-          params: {reviewId: 48}
-        }, {
-          id: 3,
-          image: 'https://xberts.imgix.net/static/banner/jorno_keyboard.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=37e53a8accf2a74e6ed34579a239b1fc',
-          title: 'Jorno Folding Bluetooth Keyboard',
-          subtitle: 'Just your type. Check out this ultra-slim compact folding keyboard.',
-          buttonShow: true,
-          buttonText: 'Get It Now',
-          buttonColor: 'btn-primary',
-          url: 'application.campaign',
-          params: {reviewId: 49}
         }
+        //, {
+        //  id: 1,
+        //  image: 'https://xberts.imgix.net/static/banner/emie_lamp.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=0f434daa97bbf3c5f68e3c57c7d7ac02',
+        //  title: 'Elfy Connected Lamp',
+        //  subtitle: 'This adorable connected lamp brings brightness, warmth and companion to you.',
+        //  buttonShow: true,
+        //  buttonText: 'Get It Now',
+        //  buttonColor: 'btn-primary',
+        //  url: 'application.campaign',
+        //  params: {reviewId: 47}
+        //}, {
+        //  id: 2,
+        //  image: 'https://xberts.imgix.net/static/banner/emie_watch.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=3019acdc204f7120f9c5a767e0b98b5c',
+        //  title: 'Nevo Balade Parisienne Smart Watch',
+        //  subtitle: 'Stay on top of your health and fitness goals, without compromising on your sense of style.',
+        //  buttonShow: true,
+        //  buttonText: 'Get It Now',
+        //  buttonColor: 'btn-primary',
+        //  url: 'application.campaign',
+        //  params: {reviewId: 48}
+        //}, {
+        //  id: 3,
+        //  image: 'https://xberts.imgix.net/static/banner/jorno_keyboard.jpg?auto=format%2Cenhance&crop=entropy&fit=crop&h=600&q=60&w=1200&s=37e53a8accf2a74e6ed34579a239b1fc',
+        //  title: 'Jorno Folding Bluetooth Keyboard',
+        //  subtitle: 'Just your type. Check out this ultra-slim compact folding keyboard.',
+        //  buttonShow: true,
+        //  buttonText: 'Get It Now',
+        //  buttonColor: 'btn-primary',
+        //  url: 'application.campaign',
+        //  params: {reviewId: 49}
+        //}
       ];
 
       $scope.slideButtonClick = function (slide) {
