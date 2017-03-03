24931d1f0b1011b80d261b04ed8634f7701ef6c6
zhao <zhaoyudong_0129@126.com>
2017-03-03 17:34:26 +0800

    test"

--- app/scripts/components/comments/feedbackService.js
+++ app/scripts/components/comments/feedbackService.js
@@ -1,7 +1,7 @@
 angular.module('xbertsApp')
   .service('FeedbackService', ['$resource', 'API_BASE_URL', 'Feedback', function ($resource, API_BASE_URL, Feedback) {
 
-    var FeedbackResource = $resource(API_BASE_URL + '/interact/feedbacks/:feedbackId/', null);
+    var FeedbackResource = $resource(API_BASE_URL + '/interact/feedbacks/:id/', null);
 
     this.getList = function (params) {
       return FeedbackResource.get(params).$promise.then(Feedback.buildPageList);
