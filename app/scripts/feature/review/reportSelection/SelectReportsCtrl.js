e8365d7368b8557d3185fc52d29fa344f8192fd0
zhao <zhaoyudong_0129@126.com>
2017-03-09 17:22:06 +0800

    fixed selectReport count debug

--- app/scripts/feature/review/reportSelection/SelectReportsCtrl.js
+++ app/scripts/feature/review/reportSelection/SelectReportsCtrl.js
@@ -5,8 +5,8 @@ angular.module('xbertsApp')
     $rootScope.pageSettings.setBackgroundColor('background-whitem');
     $scope.review = review;
     $scope.applicantPaginator = selectedApplicantPaginator;
-    $scope.applicantCount = selectedApplicantPaginator.getCount();
-    $scope.submittedApplicantCount = submittedApplicantPaginator.getCount();
+    $scope.applicantCount = selectedApplicantPaginator.count;
+    $scope.submittedApplicantCount = submittedApplicantPaginator.count;
 
     if ($rootScope.user.getUserId() != review.owner_id && !$rootScope.user.isStaff()) {
       $state.go('application.main')
