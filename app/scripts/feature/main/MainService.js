84b9a5388ee2b5459dfa5de267b3ee3031b01687
heyaohua <hyh2017312@gmail.com>
2017-03-24 13:26:05 +0800

    [MainPage]:[discoverProductsList]:change the display of video
    [MainPage]:[reviewsList]:change the number of the monitor reviews
    
    [MainPage]:[toBanner]:change the structure and the content of the top banner
    
    [MainPage]:[MainService]:add the top banner service
    
    [MainPage]:[module]:add the top banner resolve
    
    [navbar]:[_navbar]:change the style of navigation
    
    [footer]:[_footer]:change the style of footer

--- app/scripts/feature/main/MainService.js
+++ app/scripts/feature/main/MainService.js
@@ -1,7 +1,13 @@
 angular.module('xbertsApp')
   .service('MainService', ['$resource','MainModel','API_BASE_URL',function ($resource, MainModel, API_BASE_URL) {
 
+    var BannerResource = $resource(API_BASE_URL + '/contents/mainPageTopBanners/',null);
     var ReportResource = $resource(API_BASE_URL + '/review/reports/:id/');
+
+    this.getBannerList = function(params) {
+      return BannerResource.query(params).$promise.then(MainModel.buildList);
+    };
+
     this.getList = function (params) {
       return $resource(API_BASE_URL + '/review/reviews/', {id: '@id'}).get(params).$promise.then(MainModel.buildPageList);
     };
