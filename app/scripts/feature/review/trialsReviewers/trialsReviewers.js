aa36e02befbb8b5171be66446228000e2e666775
heyaohua <hyh2017312@gmail.com>
2017-04-22 22:39:56 +0800

    [trialDetail]:[trialReviewers]:remove trial console

--- app/scripts/feature/review/trialsReviewers/trialsReviewers.js
+++ app/scripts/feature/review/trialsReviewers/trialsReviewers.js
@@ -10,7 +10,6 @@ angular.module('xbertsApp')
       templateUrl: 'scripts/feature/review/trialsReviewers/trials-reviewers.html',
       link: function (scope, element, attrs, ctrls) {
         scope.reviewers = scope.reviewers || [];
-        console.log(scope.reviewers )
       }
     }
   }]);
