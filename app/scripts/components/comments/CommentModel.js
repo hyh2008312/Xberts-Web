249fe268e25ca8dee9f5dd28d4be9c5c8d4be495
heyaohua <hyh2017312@gmail.com>
2017-04-26 15:55:03 +0800

    [Comment]:[avatarImg]:change to new capital avatar when there's no picture in avatar

--- app/scripts/components/comments/CommentModel.js
+++ app/scripts/components/comments/CommentModel.js
@@ -4,19 +4,16 @@ angular.module('xbertsApp')
 
 function CommentModel() {
 
-  var AVATAR_PLACEHOLDER = 'https://xberts.imgix.net/static/icon/avatar_empty.gif?s=5b6b11a25bfa12e3a94966eb077ef16a';
-
-
   function Comment(data) {
     angular.extend(this, data);
   }
 
   Comment.prototype = {
     getPostAvatar: function () {
-      return this.post.joiner.avatar || AVATAR_PLACEHOLDER
+      return this.post.joiner.avatar;
     },
     getPostName: function () {
-      return this.post.joiner.first_name
+      return this.post.joiner.first_name;
     },
     getPostUserId: function () {
       return this.post.joiner.id;
@@ -25,10 +22,10 @@ function CommentModel() {
       return this.post.id;
     },
     getPostToAvatar: function () {
-      return this.post_to.joiner.avatar || AVATAR_PLACEHOLDER
+      return this.post_to.joiner.avatar;
     },
     getPostToName: function () {
-      return this.post_to.joiner.first_name
+      return this.post_to.joiner.first_name;
     },
     getPostToUserId: function () {
       return this.post_to.joiner.id;
--- app/scripts/components/comments/FeedbackModel.js
+++ app/scripts/components/comments/FeedbackModel.js
@@ -2,8 +2,6 @@ angular.module('xbertsApp')
   .factory('Feedback', ['Comment',FeedbackModel]);
 function FeedbackModel(Comment) {
 
-  var AVATAR_PLACEHOLDER = 'https://xberts.imgix.net/static/icon/avatar_empty.gif?s=5b6b11a25bfa12e3a94966eb077ef16a';
-
 
   function Feedback(data) {
     angular.extend(this, data);
@@ -11,7 +9,7 @@ function FeedbackModel(Comment) {
 
   Feedback.prototype = {
     getPostAvatar: function () {
-      return this.post.joiner.avatar || AVATAR_PLACEHOLDER;
+      return this.post.joiner.avatar;
     },
     getPostName: function () {
       return this.post.joiner.first_name;
