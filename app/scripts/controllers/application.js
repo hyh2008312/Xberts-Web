9785c1cc5c08d437c0210ef6382cbec05346d2f2
heyaohua <hyh2017312@gmail.com>
2017-04-28 16:17:15 +0800

    [_xb-md-supplement]:[xb-loading-top]:decrease the height of mobile

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -82,7 +82,6 @@ angular.module('xbertsApp')
       $scope.close = function () {
         // Component lookup should always be available since we are not using `ng-if`
         $mdSidenav('left').close();
-
       };
 
       $scope.isPopupOpen = false;
