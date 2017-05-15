9a306e8031739948e0d6ed281ef70b8990cbd91f
heyaohua <hyh2017312@gmail.com>
2017-05-15 16:47:21 +0800

    [Interact]:[Feedback]:add limits of comments

--- app/scripts/components/comments/feedbackList.js
+++ app/scripts/components/comments/feedbackList.js
@@ -30,7 +30,7 @@ angular.module('xbertsApp')
           if(interactCtrl.getCurrentJoin().feedback_amount >= 3) {
             scope.formToggle = !scope.formToggle;
             scope.newFeedback = {};
-            growl.error('comments');
+            growl.error("Sorry, you can't post more than 3 comments here");
             return false;
           }
           if (feedbackForm.$valid) {
