6c3055c661dec33cf08db336e817df679abf79b0
heyaohua <hyh2017312@gmail.com>
2017-05-15 17:22:35 +0800

    [Apply now]:correct the structure bugs.

--- app/scripts/utils/browser.js
+++ app/scripts/utils/browser.js
@@ -14,9 +14,8 @@ angular.module('xbertsApp')
       }
     };
     this.isFacebookApp = function() {
-      var ua = navigator.userAgent || navigator.vendor || window.opera;
+      var ua = navigator.userAgent || navigator.vendor || $window.opera;
 
-      alert((ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1));
       return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
     };
   }]);
