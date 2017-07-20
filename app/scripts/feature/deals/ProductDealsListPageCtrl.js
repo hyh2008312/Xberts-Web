d107641182ec48c8b669632f9bac655923cab068
heyaohua <hyh2017312@gmail.com>
2017-07-20 14:41:09 +0800

    [Deals]:[ProductDealsListPageCtrl]:correct the deals title

--- app/scripts/feature/deals/ProductDealsListPageCtrl.js
+++ app/scripts/feature/deals/ProductDealsListPageCtrl.js
@@ -122,7 +122,7 @@ angular.module('xbertsApp')
       $mdSidenav('filterLeft').close();
     };
 
-    var title = 'Discover - Exclusive Deals by Xberts Community';
+    var title = 'Discover - Exclusive Deals Curated by Community';
     var description = 'Discover the latest products and deals for a better life';
     var backgroundColor = 'background-bg-light';
     var shareImage = '';
