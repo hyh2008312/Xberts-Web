816d60e48ea3f627bd4ad67b64f08359a91f6604
heyaohua <hyh2017312@gmail.com>
2017-08-15 23:43:36 +0800

    [Deals]:correct the deals list page

--- app/scripts/feature/deals/ProductDealsListPageCtrl.js
+++ app/scripts/feature/deals/ProductDealsListPageCtrl.js
@@ -2,9 +2,9 @@
 
 angular.module('xbertsApp')
   .controller('ProductDealsListPageCtrl', ['$window','$rootScope', 'DealsService', 'ShareProductService',
-    '$mdSidenav','$timeout','$state','DealsFactory','$scope','ProductDeals','category','Paginator','$stateParams',
+    '$mdSidenav','$state','DealsFactory','$scope','ProductDeals','category','Paginator','$stateParams',
     function ($window, $rootScope, DealsService, ShareProductService, $mdSidenav,
-              $timeout,$state,DealsFactory,$scope,ProductDeals,category,Paginator, $stateParams) {
+              $state,DealsFactory,$scope,ProductDeals,category,Paginator, $stateParams) {
     var dealsCtrl = this;
 
     DealsService.getCategoryList = DealsFactory.getCategory(category);
