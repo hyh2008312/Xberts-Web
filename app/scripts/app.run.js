39144ee317a30b1468a1fe94ddde4a978d3fff28
heyaohua <hyh2017312@gmail.com>
2017-08-25 16:32:29 +0800

    [Address]:correct the words of address

--- app/scripts/app.run.js
+++ app/scripts/app.run.js
@@ -183,7 +183,10 @@ angular
       $rootScope.$on('$viewContentLoading', function() {
         if(angular.element(".xb-body-view").length>0)
         {
-          angular.element(".xb-body-view").scrollTop(0);
+          if($rootScope.state.current.name != 'application.askQuestionMain.answerQuestionDetail'
+            && $rootScope.state.current.name != 'application.askQuestionMain') {
+            angular.element(".xb-body-view").scrollTop(0);
+          }
         }
       });
 
