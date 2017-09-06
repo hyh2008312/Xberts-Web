1ad3e27ddecc8e4b3bfff762305698337a41ef4b
heyaohua <hyh2017312@gmail.com>
2017-09-06 17:36:30 +0800

    [Grunt]:correct the gruntfile flow

--- web.js
+++ web.js
@@ -20,9 +20,10 @@ function start() {
   app.use(morgan('dev'));
   app.use('*', function(req, res, next) {
 
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     var shouldRedirect = false;
     var host = req.get('Host');
