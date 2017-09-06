f4c3b7cffe21104db028280049e2cef3e63e92f8
heyaohua <hyh2017312@gmail.com>
2017-09-06 19:02:53 +0800

    [Grunt]:correct the gruntfile flow

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ function start() {
   app.use(morgan('dev'));
   app.use('*', function(req, res, next) {
 
-    res.header("Access-Control-Allow-Origin", "https://origin.xberts.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Credentials", "true");
