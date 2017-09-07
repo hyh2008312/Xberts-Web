e25f970aee0002f92d9e4961434ee0c51b8753dc
heyaohua <hyh2017312@gmail.com>
2017-09-07 15:49:10 +0800

    [Follow]:correct the follow

--- web.js
+++ web.js
@@ -21,7 +21,7 @@ function start() {
   app.use('*', function(req, res, next) {
 
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS');
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Credentials", "true");
 
