89286a9975be125baf37f4928d51d363125f9154
heyaohua <hyh2017312@gmail.com>
2017-09-11 14:51:05 +0800

    [Ask]:correct the ask main page display

---
+++ app/scripts/feature/loading/discover/loadDisoverMainMore.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('xbertsApp')
+  .directive('loadDiscoverMainMore', function () {
+    return {
+      templateUrl: 'scripts/feature/loading/discover/load-discover-main-more.html',
+      restrict: 'E',
+      replace: false,
+      scope: {
+
+      }
+    };
+  });
